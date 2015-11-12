77c4a9661808cba6953fd74b3ef36845bda001ce
Michael Kelly <michael@mikeryankelly.com>
2015-11-11 18:25:44 -0600

    Removing files for part-1

--- src/contants/TodoConstants.js
+++
@@ -1,5 +0,0 @@
-export const TODO_LIST = 'TODO_LIST';
-export const TODO_GET = 'TODO_GET';
-export const TODO_CREATE = 'TODO_CREATE';
-export const TODO_UPDATE = 'TODO_UPDATE';
-export const TODO_DELETE = 'TODO_DELETE';
--- src/index.jsx
+++
@@ -1,9 +0,0 @@
-import './index.scss';
-import React from 'react';
-import ReactDom from 'react-dom';
-import App from './components/App/App';
-
-ReactDom.render(
-  <App />,
-  document.getElementById('app')
-);
